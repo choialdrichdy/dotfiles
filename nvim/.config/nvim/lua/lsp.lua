2eb335aa2a1121428f61b8e53bac444e6f659e34
Aldrich Choi <choi.aldrich@gmail.com>
2026-05-25 18:24:11 +0900

    nvim: format

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,45 +1,46 @@
 local servers = {
-    -- Web
-    vtsls    = {},
-    eslint   = {},
-    html     = {},
-    cssls    = {},
-    jsonls   = {},
-    -- Markdown
-    marksman = {},
-    -- Go
-    gopls = {},
-    -- Python
-    basedpyright = {},
-    ruff         = {},
-    -- Lua
-    lua_ls = {
-        settings = {
-            Lua = {
-                diagnostics = { globals = { "vim" } },
-            },
-        },
-    },
-    -- IaC
-    dockerls                        = {},
-    docker_compose_language_service = {},
-    yamlls = {
-        settings = {
-            yaml = {
-                schemas = {
-                    kubernetes = "/*.yaml",
-                },
-            },
-        },
-    },
-    terraformls = {},
-    ansiblels   = {},
+	-- Web
+	vtsls = {},
+	eslint = {},
+	html = {},
+	cssls = {},
+	jsonls = {},
+	-- Markdown
+	marksman = {},
+	-- Go
+	gopls = {},
+	-- Python
+	basedpyright = {},
+	ruff = {},
+	-- Lua
+	lua_ls = {
+		settings = {
+			Lua = {
+				diagnostics = { globals = { "vim" } },
+			},
+		},
+	},
+	-- IaC
+	dockerls = {},
+	docker_compose_language_service = {},
+	yamlls = {
+		settings = {
+			yaml = {
+				schemas = {
+					kubernetes = "/*.yaml",
+				},
+			},
+		},
+	},
+	terraformls = {},
+	ansiblels = {},
 }
 
 require("mason").setup()
 
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "<leader>f", function()
-    require("conform").format({ async = true, lsp_format = "fallback" })
+	require("conform").format({ async = true, lsp_format = "fallback" })
 end, { desc = "Format buffer" })
 vim.keymap.set("n", "df", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
@@ -51,9 +52,9 @@ capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").g
 vim.lsp.config("*", { capabilities = capabilities })
 
 for server, config in pairs(servers) do
-    if next(config) ~= nil then
-        vim.lsp.config(server, config)
-    end
+	if next(config) ~= nil then
+		vim.lsp.config(server, config)
+	end
 end
 
 vim.lsp.enable(vim.tbl_keys(servers))
