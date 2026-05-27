685148181b924fc561f4ead039ba83f9beff3c7d
Aldrich Choi <choi.aldrich@gmail.com>
2026-05-27 21:41:49 +0900

    tmux/nvim: adds tmux config and vim tmux navigator

--- nvim/.config/nvim/lua/pack.lua
+++ nvim/.config/nvim/lua/pack.lua
@@ -1,36 +1,37 @@
 vim.pack.add({
-    "https://github.com/nvim-mini/mini.nvim",
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main", build = ":TSUpdate" },
-    "https://github.com/neovim/nvim-lspconfig",
-    "https://github.com/mason-org/mason.nvim",
-    { src = "https://github.com/saghen/blink.cmp", version = "v1" },
-    "https://github.com/stevearc/conform.nvim",
+	"https://github.com/nvim-mini/mini.nvim",
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main", build = ":TSUpdate" },
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+	{ src = "https://github.com/saghen/blink.cmp", version = "v1" },
+	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/christoomey/vim-tmux-navigator",
 })
 
 -- mini files
 local MiniFiles = require("mini.files")
 MiniFiles.setup({
-    mappings = {
-        go_in = "<CR>",
-        go_in_plus = "L",
-        go_out = "-",
-        go_out_plus = "H",
-    },
+	mappings = {
+		go_in = "<CR>",
+		go_in_plus = "L",
+		go_out = "-",
+		go_out_plus = "H",
+	},
 })
 
 vim.keymap.set("n", "<leader>-", function()
-    MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
-    MiniFiles.reveal_cwd()
+	MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+	MiniFiles.reveal_cwd()
 end, { desc = "Toggle into currently opened file" })
 
 -- mini notify
 require("mini.notify").setup({
-    -- only show messages
-    content = {
-        format = function(notif)
-            return notif.msg
-        end,
-    },
+	-- only show messages
+	content = {
+		format = function(notif)
+			return notif.msg
+		end,
+	},
 })
 
 -- mini cmdline completion
@@ -53,30 +54,30 @@ require("mini.pairs").setup()
 
 -- blink.cmp
 require("blink.cmp").setup({
-    keymap = { preset = "default" },
-    sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-    },
-    signature = { enabled = true }
+	keymap = { preset = "default" },
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+	},
+	signature = { enabled = true },
 })
 
 -- conform.nvim
 require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "ruff_format" },
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-        typescript = { "prettierd", "prettier", stop_after_first = true },
-        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-        go = { "goimports", "gofumpt" },
-        json = { "prettierd", "prettier", stop_after_first = true },
-        yaml = { "prettierd", "prettier", stop_after_first = true },
-        markdown = { "prettierd", "prettier", stop_after_first = true },
-        terraform = { "terraform_fmt" },
-    },
-    format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-    },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "ruff_format" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+		go = { "goimports", "gofumpt" },
+		json = { "prettierd", "prettier", stop_after_first = true },
+		yaml = { "prettierd", "prettier", stop_after_first = true },
+		markdown = { "prettierd", "prettier", stop_after_first = true },
+		terraform = { "terraform_fmt" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
 })
