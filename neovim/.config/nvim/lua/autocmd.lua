1317c33e370d7895420b0945065ff21e5ca62ebf
Aldrich Choi <choi.aldrich@gmail.com>
2026-06-28 16:24:51 +0900

    chore(neovim): renames nvim folder to neovim

---
+++ neovim/.config/nvim/lua/autocmd.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('wan-highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
+
---
+++ neovim/.config/nvim/lua/pack.lua
@@ -0,0 +1,86 @@
+vim.pack.add({
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+	"https://github.com/nvim-mini/mini.nvim",
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main", build = ":TSUpdate" },
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+	{ src = "https://github.com/saghen/blink.cmp", version = "v1" },
+	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/christoomey/vim-tmux-navigator",
+})
+
+-- mini files
+local MiniFiles = require("mini.files")
+MiniFiles.setup({
+	mappings = {
+		go_in = "<CR>",
+		go_in_plus = "L",
+		go_out = "-",
+		go_out_plus = "H",
+	},
+})
+
+vim.keymap.set("n", "<leader>-", function()
+	MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+	MiniFiles.reveal_cwd()
+end, { desc = "Toggle into currently opened file" })
+
+-- mini notify
+require("mini.notify").setup({
+	-- only show messages
+	content = {
+		format = function(notif)
+			return notif.msg
+		end,
+	},
+})
+
+-- mini cmdline completion
+require("mini.cmdline").setup()
+
+-- mini surround
+require("mini.surround").setup()
+-- Default Keymaps
+-- | `sa` | Add surrounding or Direct with 'saiw' |
+-- | `sd` | Delete surrounding |
+-- | `sr` | Replace surrounding |
+-- | `sf` | Find surrounding (right) |
+-- | `sF` | Find surrounding (left) |
+-- | `sh` | Highlight surrounding |
+-- | `sn` | Update n_lines |
+-- | `l` / `n` | as suffix for prev/next |
+
+-- mini pairs
+require("mini.pairs").setup()
+
+-- blink.cmp
+require("blink.cmp").setup({
+	keymap = { preset = "default" },
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+	},
+	signature = { enabled = true },
+})
+
+-- conform.nvim
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "ruff_format" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+		go = { "goimports", "gofumpt" },
+		json = { "prettierd", "prettier", stop_after_first = true },
+		yaml = { "prettierd", "prettier", stop_after_first = true },
+		markdown = { "prettierd", "prettier", stop_after_first = true },
+		terraform = { "tofu_fmt", "terraform_fmt", stop_after_first = true },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+
+vim.cmd.colorscheme("catppuccin-mocha")
