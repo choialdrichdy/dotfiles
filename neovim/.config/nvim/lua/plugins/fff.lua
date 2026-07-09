5ec45e8ae69130880129eb075261001145f37677
Aldrich Choi <aldrich@numbahwan-osx.lan>
2026-07-09 23:26:12 +0900

    chore(neovim): adds more fff keybinds

--- neovim/.config/nvim/lua/plugins/fff.lua
+++ neovim/.config/nvim/lua/plugins/fff.lua
@@ -8,3 +8,22 @@ vim.g.fff = {
 vim.keymap.set("n", "ff", function()
 	require("fff").find_files()
 end, { desc = "FFFind files" })
+
+vim.keymap.set("n", "fg", function()
+	require("fff").live_grep()
+end, { desc = "LiFFFe grep" })
+
+vim.keymap.set("n", "fz", function()
+	require("fff").live_grep({
+		grep = {
+			modes = {
+				"fuzzy",
+				"plain",
+			},
+		},
+	})
+end, { desc = "Live fffuzzy grep" })
+
+vim.keymap.set("n", "fw", function()
+	require("fff").live_grep_under_cursor()
+end, { desc = "Search current word / selection" })
